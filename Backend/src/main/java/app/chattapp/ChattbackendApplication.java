37fb510826a914da8435b68df087497d358e2da2
Artur Waliczek <artur.waliczek@gmail.com>
2018-01-18 21:15:39 +0100

    Init backend

---
+++ Backend/src/main/java/app/chattapp/ChattbackendApplication.java
@@ -0,0 +1,12 @@
+package app.chattapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattbackendApplication.class, args);
+	}
+}
